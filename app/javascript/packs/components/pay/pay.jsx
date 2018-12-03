9faa37ce1af98c47e01372937b0858f1f1f5b1f1
claraonmars <claraonmars@hotmail.com>
2018-12-03 18:16:33 +0800

    Pay via QR code — working

--- app/javascript/packs/components/pay/pay.jsx
+++ app/javascript/packs/components/pay/pay.jsx
@@ -17,34 +17,22 @@ import 'whatwg-fetch';
 
 
 class Test extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      delay: 300,
-      result: "No result"
-    };
-    this.handleScan = this.handleScan.bind(this);
-  }
-  handleScan(data) {
-    if (data) {
-      this.setState({
-        result: data
-      });
-    }
-  }
-  handleError(err) {
+
+
+handleError(err) {
     console.error(err);
   }
+
   render() {
     return (
       <div>
         <QrReader
-          delay={this.state.delay}
+          delay={this.props.delay}
           onError={this.handleError}
-          onScan={this.handleScan}
+          onScan={this.props.handleScan}
           style={{ width: "100%" }}
         />
-        <p>{this.state.result}</p>
+        <p>{this.props.message}</p>
       </div>
     );
   }
@@ -56,7 +44,6 @@ class Payform extends React.Component{
 render(){
 
     return(<div>
-        <BrowserView>
         <div className="row justify-content-center">
             <h1>Make a payment to another account</h1>
             </div>
@@ -91,12 +78,8 @@ render(){
             <div className="row justify-content-center">
                 <Button size="sm" onClick={this.props.submitform}>Make payment now</Button>
             </div>
-            </BrowserView>
 
-            <MobileView>
-                Scan to Pay
-                <Test/>
-            </MobileView>
+
 
         </div>)
     }
@@ -138,12 +121,19 @@ constructor(){
         payee: '',
         payee_acc: '',
 
+        delay: 300,
+        result: {},
+        message:'',
+
+        once: 0,
+
     }
 
     this.getAmount = this.getAmount.bind(this)
     this.getPayee = this.getPayee.bind(this)
     this.getPayeeAcc = this.getPayeeAcc.bind(this)
     this.submitform = this.submitform.bind(this)
+    this.handleScan = this.handleScan.bind(this);
 
 }
 
@@ -331,6 +321,222 @@ constructor(){
         console.log(event.target.value)
     }
 
+
+
+    handleScan(data) {
+    if (data) {
+      this.setState({
+        result: {data},
+        message:'Payment successful',
+        once: this.state.once + 1,
+      });
+    }
+  }
+
+  componentDidUpdate(prevState){
+    if(this.state.result != prevState.result && this.state.once === 1){
+
+        console.log(this.state.once)
+
+        // Hard coding QR code data
+        var qrSplit = this.state.result.data.split(',')
+        var qrAmt = parseInt(qrSplit[0])
+        var qrAcc = parseInt(qrSplit[1])
+        var qrMerchant = parseInt(qrSplit[2])
+
+       ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+      //FIND A BETTER WAY INSTEAD OF REPEATING
+      ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        var reactThis = this
+        let newcurrenttransc
+        let newsavingtransc
+        let investmenttransc
+
+        ////////////////////////////////////////////////////////////
+        //Check for existing investment plans//
+        ////////////////////////////////////////////////////////////
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        //Rounding savings//
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        if (reactThis.state.investments.rounding === true){
+
+            ////////////////////////////////////////////////////////////
+            //Check if amount is decimal//
+            ////////////////////////////////////////////////////////////
+            if(qrAmt % 1 != 0){
+
+                newcurrenttransc = {
+                    current:{
+                        amount: qrAmt,
+                        balance: parseFloat(reactThis.state.transaction.current.balance) - parseFloat(qrAmt),
+                        sort: 'DEBIT',
+                        operation: 'Payment to ' + qrAcc.toString(),
+                        date: new Date(),
+                        account_id: reactThis.state.transaction.current.account_id,
+                        merchant_id: 1
+                    }
+                }
+
+                newsavingtransc = {
+                    saving:{
+                        amount: Math.round(qrAmt) - qrAmt,
+                        balance: parseFloat(reactThis.state.transaction.saving.balance) + parseFloat(Math.round(qrAmt) - qrAmt),
+                        sort: 'CREDIT',
+                        operation: 'Rounding Investment',
+                        date: new Date(),
+                        account_id: reactThis.state.transaction.saving.account_id
+                    }
+                }
+
+            }
+
+            else{
+
+                newcurrenttransc = {
+                    current:{
+                        amount: qrAmt,
+                        balance: parseFloat(reactThis.state.transaction.current.balance) - qrAmt,
+                        sort: 'DEBIT',
+                        operation: 'Payment to ' + qrAcc.toString(),
+                        date: new Date(),
+                        account_id: reactThis.state.transaction.current.account_id,
+                        merchant_id: 1
+                    }
+                }
+
+            }
+
+        }
+
+        ////////////////////////////////////////////////////////////
+        //Continue with transaction without investment plan//
+        ////////////////////////////////////////////////////////////
+
+        else{
+
+            //update transaction details without investment plans
+            newcurrenttransc = {
+                current:{
+                    amount: qrAmt,
+                    balance: parseFloat(reactThis.state.transaction.current.balance) - qrAmt,
+                    sort: 'DEBIT',
+                    operation: 'Payment to ',
+                    date: new Date(),
+                    account_id: reactThis.state.transaction.current.account_id,
+                    merchant_id: 0
+                }
+            }
+        }
+
+
+        ////////////////////////////////////////////////////////////
+        //Posting trans to database//
+        ////////////////////////////////////////////////////////////
+
+        //make a new transaction to transfer $ from current ...
+
+        fetch('/currents',{
+                method: 'post',
+                body: JSON.stringify(newcurrenttransc),
+                headers : {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                   }
+            })
+            .then(function(response){
+                return response.json()
+            })
+            .then(function(data){
+                console.log('current',newcurrenttransc);
+                console.log('data',data);
+
+                    //get updated current transactions
+                    fetch('/accounts/1/currents',{
+                        method: 'get',
+                        headers : {
+                            'Content-Type': 'application/json',
+                            'Accept': 'application/json'
+                           }
+                    })
+                    .then(function(response){
+                        return response.json()
+                    })
+                    .then(function(data){
+                        console.log('currents req', data);
+
+                        var total = data.length
+
+                        //getting latest transaction details
+                        //console.log(data[total-1])
+
+                         //set state of current account
+                        var current = reactThis.state.transaction
+                        current.current = data[total-1]
+                        reactThis.setState({transaction: current})
+
+                            investmenttransc = {
+                                current:{
+                                    amount: (Math.round(qrAmt) - qrAmt),
+                                    balance: parseFloat(reactThis.state.transaction.current.balance) - parseFloat(Math.round(qrAmt) - qrAmt),
+                                    sort: 'DEBIT',
+                                    operation: 'Rounding investment',
+                                    date: new Date(),
+                                    account_id: reactThis.state.transaction.current.account_id,
+                                    merchant_id: 0
+                                }
+                            }
+
+
+                            if (investmenttransc != null && investmenttransc.current.amount != 0){
+                                fetch('/currents/',{
+                                    method: 'post',
+                                    body: JSON.stringify(investmenttransc),
+                                    headers : {
+                                        'Content-Type': 'application/json',
+                                        'Accept': 'application/json'
+                                       }
+                                })
+                                .then(function(response){
+                                    return response.json()
+                                })
+                                .then(function(data){
+                                    console.log('current',investmenttransc);
+                                    console.log('data',data);
+                                })
+
+                            }
+
+
+                    })
+            })
+
+        // ....to savings
+
+        if (newsavingtransc != null && newsavingtransc.saving.amount != 0){
+        fetch('/savings/',{
+                method: 'post',
+                body: JSON.stringify(newsavingtransc),
+                headers : {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                   }
+            })
+            .then(function(response){
+                return response.json()
+            })
+            .then(function(data){
+                console.log('saving', newsavingtransc);
+                console.log('data',data);
+            })
+        }
+
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+      }
+  }
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Confirm payment//
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -380,6 +586,22 @@ constructor(){
                 }
 
             }
+
+            else{
+
+            //update transaction details without investment plans
+            newcurrenttransc = {
+                current:{
+                    amount: reactThis.state.payer_amt,
+                    balance: parseFloat(reactThis.state.transaction.current.balance) - parseFloat(reactThis.state.payer_amt),
+                    sort: 'DEBIT',
+                    operation: 'Payment to '+ reactThis.state.payee_acc,
+                    date: new Date(),
+                    account_id: reactThis.state.transaction.current.account_id,
+                    merchant_id: 0
+                }
+            }
+        }
 
         }
 
@@ -512,6 +734,7 @@ render(){
     return(
         <div>
             <h1>Payment Gate</h1>
+            <BrowserView>
             <Payform
             payer_amt={this.state.payer_amt}
             payee={this.state.payee}
@@ -520,6 +743,16 @@ render(){
             getPayee={this.getPayee}
             getPayeeAcc={this.getPayeeAcc}
             submitform={this.submitform} />
+            </BrowserView>
+
+            <MobileView>
+                Scan to Pay
+                <Test
+                delay={this.state.delay}
+                result={this.state.result}
+                message={this.state.message}
+                handleScan={this.handleScan}/>
+            </MobileView>
         </div>
         )
 }
